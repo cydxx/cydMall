89f24b626da3efb17c635f11519257879271a168
cyd <cydvvxx@sina.com>
2020-08-11 01:41:17 +0800

    商品分类接口

---
+++ cydmall-gateway/src/main/java/com/cydvv/cydmall/lgateway/config/CrosConfiguation.java
@@ -0,0 +1,25 @@
+package com.cydvv.cydmall.lgateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.server.ServerWebExchange;
+
+@Configuration
+public class CrosConfiguation {
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        UrlBasedCorsConfigurationSource resource = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+        resource.registerCorsConfiguration("/**",corsConfiguration);
+        return new CorsWebFilter(resource);
+    }
+}
